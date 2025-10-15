3bb74d851a3b2fc896d56792308bd2794443dce7
Nicolas Bravo <nicolasalejandro.bravo@estudiantes.unahur.edu.ar>
2025-10-14 21:55:39 -0300

    Actualizacion interfaz prestadores

--- src/types/prestadores.d.ts
+++ src/types/prestadores.d.ts
@@ -1,52 +1,4 @@
-interface Direccion {
-  calle: string;
-  numero: string;
-  localidad: string;
-  codigoPostal: string | null;
-  horariosAtencion: HorarioAtencion[];
-}
-
-interface HorarioAtencion {
-  dia: string;
-  desde: string;
-  hasta: string;
-  duracionTurno: string;
-}
-interface Prestador {
-  id: number;
-  numeroCUIL: string;export interface HorarioAtencion {
-    dia: string;
-    desde: string;
-    hasta: string;
-    duracionTurno: string;
-}
-
-export interface Direccion {
-    calle: string;
-    numero: string;
-    localidad: string;
-    codigoPostal: string;
-    horariosAtencion: HorarioAtencion[];
-}
-
-export interface Prestador {
-    id: number;
-    numeroCUIL: string;
-    nombreCompleto: string;
-    especialidades: Especialidad[];
-    esProfesionalIndependiente: boolean;
-    telefono: string[];
-    email: string[];
-    direccion: Direccion[];
-}
-
-export interface Especialidad {
-    id: number;
-    nombre: string;
-}
-
-  nombreCompleto: string;
-  especialidades: string[];export interface HorarioAtencion {
+export interface HorarioAtencion {
     id: number;
     dia: string;
     desde: string;
@@ -78,15 +30,3 @@ export interface Especialidad {
     id: number;
     nombre: string;
 }
-
-  tipoPrestacion: string;
-  telefono: string[];
-  email: string[];
-  direccion: Direccion[]
-}
-
-interface ListaPrestadoresProps {
-    prestadores: Prestador[];
-}
-
-export { Direccion, HorarioAtencion, Prestador, ListaPrestadoresProps };
